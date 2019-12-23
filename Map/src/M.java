856507aafdacbed907accd3d4d6f318a026801a7
pbejgamwar123 <p.bejgamwar@gmail.com>
2019-12-23 10:25:46 +0530

    hi this is pallavi new repository

---
+++ Map/src/M.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+public class M 
+{
+	public static void main(String[] args) 
+	{
+		Map<Integer, String>m=new HashMap<>();
+		m.put(1, "abc");
+		m.put(2, "pqr");
+		m.put(3, "xy");
+		m.put(4, "aaa");
+		m.put(4, "xyz");
+		m.put(null, "ccc");//insertion order is not maintained
+		//System.out.println(m);
+		m.entrySet();
+		
+	
+		/* for(Map.Entry m1:m.entrySet()){  
+			   System.out.println(m1.getKey()+" "+m1.getValue());  
+			  }  */
+	}
+
+}
